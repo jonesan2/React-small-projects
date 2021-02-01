ae3c80a53568879f2cc7385f4675567124fa03d5
Andrew Jones <andrewrayjones@gmail.com>
2021-02-01 07:04:00 -0600

    extracted backend communication into a module

--- part0/src/App.js
+++ part0/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Note from './components/Note';
+import noteService from './services/notes';
 
 const App = () => {
   const [notes, setNotes] = useState([]);
@@ -21,16 +22,44 @@ const App = () => {
       content: newNote,
       date: new Date().toISOString(),
       important: Math.random() < 0.5,
-      id: notes.length + 1,
     }
 
-    setNotes(notes.concat(noteObject));
-    setNewNote('');
+    noteService
+      .create(noteObject)
+      .then(returnedNote => {
+        setNotes(notes.concat(returnedNote));
+        setNewNote('');
+      });
   };
 
   const handleNoteChange = (event) => {
     setNewNote(event.target.value);
   };
+
+  useEffect(() => {
+    noteService
+      .getAll()
+      .then(initialNotes => {
+        setNotes(initialNotes)
+      });
+  }, []);
+
+  const toggleImportanceOf = (id) => {
+    const note = notes.find(n => n.id === id);
+    const changedNote = { ...note, important: !note.important }
+
+    noteService
+      .update(id, changedNote)
+      .then(returnedNote => {
+        setNotes(notes.map(note => note.id !== id ? note : returnedNote))
+      })
+      .catch(error => {
+        alert(
+          `the note '${note.content}' was already deleted from server`
+        );
+        setNotes(notes.filter(n => n.id !== id));
+      });
+  }
 
   const notesToShow = showAll
     ? notes
@@ -45,8 +74,12 @@ const App = () => {
         </button>
       </div>
       <ul>
-        {notesToShow.map((note) => 
-          <Note key={note.id} note={note} />
+        {notesToShow.map((note, i) => 
+          <Note 
+            key={i}
+            note={note}
+            toggleImportance={() => toggleImportanceOf(note.id)}
+          />
         )}
       </ul>
       <form onSubmit={addNote}>
