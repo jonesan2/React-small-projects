31c7919d5328cb732e69bf458a39a39a7bd4fdca
Andrew Jones <andrewrayjones@gmail.com>
2021-02-14 15:59:26 -0600

    bloglist project: fix remove button refactor

--- bloglist-frontend/src/components/Blog.js
+++ bloglist-frontend/src/components/Blog.js
@@ -20,12 +20,11 @@ const blogDetailsForm = (blog, updateLikes) => (
   </Togglable>
 )
 
-const removeButton = (blog, username, deleteBlog) => {
-  { username === blog.user.username ?
+const removeButton = (blog, username, deleteBlog) => (
+  username === blog.user.username ?
     <button className='remove' onClick={handleDelete(blog, deleteBlog)}>remove</button> :
     <></>
-  }
-}
+)
 
 const Blog = ({ blog, username, updateLikes, deleteBlog }) => {
   return (
