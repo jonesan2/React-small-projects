181bd1ec6c1f660c5559feebeac25c3145d0d7a7
Andrew Jones <andrewrayjones@gmail.com>
2021-02-14 15:55:14 -0600

    bloglist project: refactor remove button into a method

--- bloglist-frontend/src/components/Blog.js
+++ bloglist-frontend/src/components/Blog.js
@@ -20,15 +20,19 @@ const blogDetailsForm = (blog, updateLikes) => (
   </Togglable>
 )
 
+const removeButton = (blog, username, deleteBlog) => {
+  { username === blog.user.username ?
+    <button className='remove' onClick={handleDelete(blog, deleteBlog)}>remove</button> :
+    <></>
+  }
+}
+
 const Blog = ({ blog, username, updateLikes, deleteBlog }) => {
   return (
     <div className="blog">
       <p className="titleauthor">{blog.title} {blog.author}</p>
       {blogDetailsForm(blog, updateLikes)}
-      { username === blog.user.username ?
-        <button className='remove' onClick={handleDelete(blog, deleteBlog)}>remove</button> :
-        <></>
-      }
+      {removeButton(blog, username, deleteBlog)}
     </div>
   )
 }
